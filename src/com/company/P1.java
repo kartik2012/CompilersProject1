150ba8a2b2f4daea780d9abff143e21c83ad97a1
Kartik Ayalasomayajula <kayalasomayajula@shoppertrak.com>
2016-09-11 20:14:31 -0500

    Implemented file-reading functionality

--- src/com/company/P1.java
+++ src/com/company/P1.java
@@ -7,11 +7,51 @@ package com.company;
  * Project        1A
  *
  * Class:         P1.Java
- * Purpose:       Define main tester class to test functionality of Symbol Table.
+ * Purpose:       Main Function to test functionality of Sym and SymTable.
  */
+import java.io.*;
+import java.util.*;
+
 public class P1 {
 
-    public static void main(String[] args) {
-        // write your code here
+    public static void main(String[] args) throws IOException{
+        ArrayList<String> pageLines = openFile();
+
+    }
+
+    public static ArrayList<String> openFile() throws IOException{
+
+        // The name of the file to open.
+        String fileName = "resources/P1InputFile.txt";
+        ArrayList<String> pageLines = new ArrayList<String>();
+
+        // This will reference one line at a time
+        String line = null;
+
+        try {
+            // FileReader reads text files in the default encoding.
+            FileReader fr =
+                    new FileReader(fileName);
+
+            // Always wrap FileReader in BufferedReader.
+            BufferedReader bufferedReader =
+                    new BufferedReader(fr);
+
+            while((line = bufferedReader.readLine()) != null) {
+                pageLines.add(line);
+            }
+
+            // Always close files.
+            bufferedReader.close();
+        }
+        catch(FileNotFoundException ex) {
+            System.out.println("Unable to open file");
+        }
+        catch(IOException ex) {
+            System.out.println("Error reading file");
+        }
+
+        return pageLines;
+
     }
 }
